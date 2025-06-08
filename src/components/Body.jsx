75e145d53fd6104f6147bb1b5beb80efa9681820
shanvid19 <143689484+shanvid19@users.noreply.github.com>
2025-06-08 23:27:32 +0530

    Fetching data from Swiggy's API

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,25 @@
 import ResCard from "./ResCard";
 import resList from "../utils/mockData";
-import { useState } from "react"; //NAMED IMPORT
+import { useEffect, useState } from "react"; //NAMED IMPORT
 
 const Body = () => {
   const [listOfRes, setListOfRes] = useState(resList);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=26.942225&lng=80.9518745&carousel=true&third_party_vendor=1");
+    const json = await data.json();
+    console.log(json);
+  }
+
   return (
     <div className="body-section">
+
       <div className="topRated">
+
 
         <button className="topRated-btn" onClick={()=>{
           const filteredList = listOfRes.filter(
@@ -16,6 +28,7 @@ const Body = () => {
           setListOfRes(filteredList);
         }}>
           Top Rated Restaurants</button>
+
 
       </div>
 
