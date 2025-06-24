9b0c68d041caed9c6321d4f322ee661b5018222e
shanvid19 <143689484+shanvid19@users.noreply.github.com>
2025-06-24 13:16:39 +0530

    Ep 8 Concluded- Used GitHub API to fetch data in about us page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,27 +1,24 @@
-import User from "./User";
 import UserClass from "./UserClass";
 import React from "react";
 
 class AboutUs extends React.Component {
     constructor(props) {
         super(props);
-        console.log("Parent constructor")
+        //console.log("Parent constructor")
     }
 
     componentDidMount(){
-        console.log("Parent componentDidMount");
+        //console.log("Parent componentDidMount");
     }
 
     render() {
-        console.log("Parent render");
+        //console.log("Parent render");
 
         return (
             < div className="aboutUs" >
                 <h1>Welcome to About Us Page!</h1>
                 <br />
                 <UserClass name={"Child 1"} location="Lucknow, Uttar Pradesh, India" />
-                <UserClass name={"Child 2"} location="Dehradun, Uttarakhand, India" />
-                <UserClass name={"Child 3"} location="Dehradun, Uttarakhand, India" />
             </div >
         );
     }
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -6,44 +6,44 @@ class UserClass extends React.Component {
     constructor(props){
         super(props);
 
-        this.state = {
-            count : 0,
-            count2 : 1,
+        this.state = {  
+            userInfo : {  //state variable to store fetched information from api
+                name : "",
+            }
         };
 
-        console.log(this.props.name + " constructor");
+        //console.log(this.props.name + " constructor");
     }
 
-    componentDidMount(){
-        console.log(this.props.name + " componentDidMount");
+    async componentDidMount(){
+        //console.log(this.props.name + " componentDidMount");
+        const data = await fetch("https://api.github.com/users/shanvid19");
+        const json = await data.json();
+
+        //console.log(json);
+        this.setState({ //updating the stte variable with the fetched information
+            userInfo : json,
+        });
     }
 
     render() {
-        console.log(this.props.name + " render");
+        //console.log(this.props.name + " render");
 
-        const {name, location} = this.props;
+        //const {name, location} = this.props;
 
-        const{count, count2} = this.state; //destructuring
+        const{
+            name,
+            location,
+            avatar_url
+        } = this.state.userInfo;
 
         return (
             <div className="contributor-data">
-                <div className="count-btn">
-                    <button onClick={() => {
-                        this.setState({
-                            count : this.state.count + 1,
-                            count2 : this.state.count - 1,
-                        })
-                        console.log(count + " " + count2);
-                    }}>
-                        Increase
-                    </button>
-                </div>
-
-                <h1 className="contri-name">Name: {name + " (Class)"}</h1>
+                <img src={avatar_url}/>
+                <h1 className="contri-name">{name}</h1>
                 <h2 className="contri-contact">Contact: shanvid.ug23.ch@nitp.ac.in</h2>
                 <h2 className="contri-location">Location: {location}</h2>
 
-                <h4>{this.state.count}</h4>
             </div>
         );
     }
